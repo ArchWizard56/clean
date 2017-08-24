5601d0b2cf82dcccaa947b38b7c17d3a5d9ad184
xixu <happyslowly@gmail.com>
2017-08-24 09:40:05 -0700

    change git color

--- clean.zsh
+++ clean.zsh
@@ -2,9 +2,9 @@ ARROW="❯"
 local ret_status="%(?:$ARROW :%{$fg_bold[red]%}$ARROW )"
 
 PROMPT='%B%F%c $ret_status'
-RPROMPT='%B%F%{$fg[blue]%}$(git_current_branch)$(parse_git_dirty)$(git_prompt_status)%{$reset_color%}'
+RPROMPT='%B%F%{$fg[magenta]%}$(git_current_branch)$(parse_git_dirty)$(git_prompt_status)%{$reset_color%}'
 
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[red]%}✗%{$reset_color%}%B%F%{$fg[blue]%}"
-ZSH_THEME_GIT_PROMPT_AHEAD="%{$fg[yellow]%}⇡%{$reset_color%}%B%F%{$fg[blue]%}"
-ZSH_THEME_GIT_PROMPT_AHEAD_REMOTE="%{$fg[yellow]%}⇣%{$reset_color%}%B%F%{$fg[blue]%}"
+ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[red]%}✗%{$reset_color%}%B%F%{$fg[magenta]%}"
+ZSH_THEME_GIT_PROMPT_AHEAD="%{$fg[yellow]%}⇡%{$reset_color%}%B%F%{$fg[magenta]%}"
+ZSH_THEME_GIT_PROMPT_AHEAD_REMOTE="%{$fg[yellow]%}⇣%{$reset_color%}%B%F%{$fg[magenta]%}"
 ZSH_THEME_GIT_PROMPT_CLEAN=
